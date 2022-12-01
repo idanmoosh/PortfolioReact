c22ac9972c08fec8fc5475033b686cc1a4020d28
Idan Moosh Haim <idanmoosh@gmail.com>
2022-12-01 12:18:52 +0200

    deployed website

---
+++ build/precache-manifest.dc466d7c0564591d9ad62a2f14de9db6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "be2182332fb4aaa8304a",
+    "url": "/PortfolioReact/static/css/main.32bdcaae.chunk.css"
+  },
+  {
+    "revision": "be2182332fb4aaa8304a",
+    "url": "/PortfolioReact/static/js/main.2c1616c6.chunk.js"
+  },
+  {
+    "revision": "7b9d29e7ee922d27cde7",
+    "url": "/PortfolioReact/static/js/runtime~main.5a3f0ba8.js"
+  },
+  {
+    "revision": "ad19effc4a8bbef8ab36",
+    "url": "/PortfolioReact/static/js/2.4c21f01f.chunk.js"
+  },
+  {
+    "revision": "60b3172c33a8dc49665c",
+    "url": "/PortfolioReact/static/js/3.29a8a44b.chunk.js"
+  },
+  {
+    "revision": "ea20df6953adcf6ae2f57a59c671f7ab",
+    "url": "/PortfolioReact/static/media/pixels.ea20df69.jpg"
+  },
+  {
+    "revision": "419d7954140a1dca086c3dfe46fff8bc",
+    "url": "/PortfolioReact/static/media/pxArt.419d7954.png"
+  },
+  {
+    "revision": "290ed0613cbfc022512e5fe528908256",
+    "url": "/PortfolioReact/static/media/Oil_Essence.290ed061.png"
+  },
+  {
+    "revision": "4f6d1bdb49ea3dc67e5647f328ab25bc",
+    "url": "/PortfolioReact/static/media/React_Synth.4f6d1bdb.png"
+  },
+  {
+    "revision": "d2cd5850d2ea2056f45871bf2ae85f35",
+    "url": "/PortfolioReact/static/media/tournament generator.d2cd5850.jpg"
+  },
+  {
+    "revision": "8939ef2481b9338f9a0a0920072a6432",
+    "url": "/PortfolioReact/index.html"
+  }
+];
