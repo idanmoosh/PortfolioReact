f69a9659e7de8264f156642c763840ec0b5e3952
Idan Moosh Haim <idanmoosh@gmail.com>
2022-12-05 13:51:06 +0200

    added navigation to home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
 import { About } from './components/About/About';
 import { Work } from './components/Work/Work';
 import './App.css';
 import { Contact } from './components/Contact/Contact';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from 'react-router-dom';
 
 function App() {
+  let navigate = useNavigate();
+  useEffect(() => {
+    navigate('/');
+  });
   return (
     <Router>
       <div className='App'>
         <Header />
         <Routes>
-          <Route
-            path='/'
-            render={() => {
-              return <Redirect to='/' />;
-            }}
-            element={<Home />}
-          />
+          <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/work' element={<Work />} />
         </Routes>
