09eb0e6b2f9b4dc43a7454cd478f4d2c22a8253e
Idan Moosh Haim <idanmoosh@gmail.com>
2022-12-05 14:38:14 +0200

    attempting run to homepage

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import { useEffect } from 'react';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
 import { About } from './components/About/About';
 import { Work } from './components/Work/Work';
 import './App.css';
 import { Contact } from './components/Contact/Contact';
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  let navigate = useNavigate();
-  useEffect(() => {
-    navigate('/');
-  });
   return (
     <Router>
       <div className='App'>
         <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/work' element={<Work />} />
+          <Route path='/' element={<Home />} />
         </Routes>
         <Contact />
       </div>
